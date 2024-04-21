99a1a40f073bf049f39d349161ebaca28bc6ca08
xbonda06 <mediabondar@gmail.com>
2024-04-21 02:53:08 +0200

    feat: add CancelKey handle, Filter initialisation

--- ipk-sniffer/ipk-sniffer/Sniffer.cs
+++ ipk-sniffer/ipk-sniffer/Sniffer.cs
@@ -1,4 +1,5 @@
-﻿using SharpPcap.LibPcap;
+﻿using SharpPcap;
+using SharpPcap.LibPcap;
 
 namespace IPK_sniffer;
 
@@ -24,8 +25,111 @@ public class Sniffer
             Console.WriteLine($"Device {Options.Interface} not found");
             return;
         }
+
+        Console.CancelKeyPress += HandleCancelKey;
+        
+        int readTimeoutMilliseconds = 1000;
+        Device.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+        Device.Filter = FilterInit();
     }
 
+    private static string FilterInit()
+    {
+        string filter = "";
+        if(Options.Tcp)
+        {
+            filter += "(ip or ip6 and tcp ";
+            if (Options.Port == null)
+            {
+                filter += ") or ";
+            }
+            else
+            {
+                if(Options.SourceOnly)
+                {
+                    filter += $"src port {Options.Port}) or ";
+                }
+                else if (Options.DestOnly)
+                {
+                    filter += $"dst port {Options.Port}) or ";
+                }
+                else
+                {
+                    filter += $"port {Options.Port}) or ";
+                }
+            }
+        }
+        
+        if(Options.Udp)
+        {
+            filter += "(ip or ip6 and udp ";
+            if (Options.Port == null)
+            {
+                filter += ") or ";
+            }
+            else
+            {
+                if(Options.SourceOnly)
+                {
+                    filter += $"src port {Options.Port}) or ";
+                }
+                else if (Options.DestOnly)
+                {
+                    filter += $"dst port {Options.Port}) or ";
+                }
+                else
+                {
+                    filter += $"port {Options.Port}) or ";
+                }
+            }
+        }
+        
+        if(Options.Arp)
+        {
+            filter += "(arp) or ";
+        }
+        
+        if(Options.Ndp)
+        {
+            filter += "(ndp) or ";
+        }
+        
+        if(Options.Icmp4)
+        {
+            filter += "(icmp) or ";
+        }
+        
+        if(Options.Icmp6)
+        {
+            filter += "(icmp6) or ";
+        }
+        
+        if(Options.Igmp)
+        {
+            filter += "(igmp) or ";
+        }
+        
+        if(Options.Mld)
+        {
+            filter += "(mld) or ";
+        }
+        
+        if (filter.Length > 0)
+        {
+            filter = filter.Substring(0, filter.Length - 4);
+        }
+
+        return filter;
+    }
+
+    private static void HandleCancelKey(object? sender, ConsoleCancelEventArgs e)
+    {
+        Device?.StopCapture();
+        Device?.Close();
+        Console.WriteLine("Capture stopped");
+        Environment.Exit(0);
+    }
+    
     private static void ListAvailableDevices()
     {
         var devices = LibPcapLiveDeviceList.Instance;
